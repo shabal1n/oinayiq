b6f27f195527e0c7a146c8800d8de82d6d17664d
shabal1n <artur.shab@yandex.ru>
2024-04-24 10:54:34 +0500

    Fixing sign up issue

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -94,13 +94,13 @@ const RegisterModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => signIn("google", { callbackUrl: "/api/auth/callback/google" })}
+        onClick={() => signIn("google", { callbackUrl: "/" })}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => signIn("github", { callbackUrl: "/api/auth/callback/github" })}
+        onClick={() => signIn("github", { callbackUrl: "/" })}
       />
       <div
         className="
