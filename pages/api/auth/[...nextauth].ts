474c3e3e056a67d5926e7ca222b63d10a614e5fb
shabal1n <artur.shab@yandex.ru>
2024-04-24 11:26:58 +0500

    Added timeout

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -19,6 +19,9 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      httpOptions: {
+        timeout: 60000,
+      },
     }),
     CredentialsProvider({
       name: "credentials",
